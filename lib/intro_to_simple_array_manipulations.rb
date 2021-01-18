79275e80b916394d420ab79718260a457c530d8b
Marissa Nolan <ritz1484@cs.com>
2021-01-18 21:24:11 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -2,8 +2,8 @@ def using_concat(array1, array2)
   new_array = array1.concat(array2)
 end
 
-def using_instert(array, element)
-  new_element = array.insert(3,element)
+def using_insert(array, element)
+  new_element = array.insert(4,element)
   new_array = array.concat(new_element)
   puts new_array
 end
